48cad622ab5da37394ee2caf07a4b8a19525c1d9
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2023-01-04 21:20:11 +0700

    ignore emote strings

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
 #include <dpp/dpp.h>
 #include "string"
+#include "locale"
 
-int main() {
+std::locale botLocale("C");
+
+bool is_emote_string(std::string_view s)
+{
+    // <:a:1>
+    auto len = s.length();
+    if (len < 6) return false;
+    if (s[0] != '<') return false;
+    if (s[1] != ':') return false;
+    if (s[s.length() - 1] != '>') return false;
+    
+    auto next = 0;
+    for (int i = 2; i < len - 1; ++i)
+    {
+        auto c = s[i];
+        if (c == ':')
+        {
+            next = i + 1;
+            break;
+        }
+        if (!std::islower(c, botLocale) && !std::isupper(c, botLocale) && !std::isdigit(c) && c != '_') {
+            return false;
+        }
+    }
+    
+    for (int i = next; i < len - 1; ++i)
+    {
+        if (!std::isdigit(s[i])) return false;
+    }
+    
+    return true;
+}
+
+int main()
+{
     auto token = getenv("DISCORD_TOKEN");
     dpp::cluster bot(token);
     bot.intents |= dpp::i_message_content;
@@ -10,21 +45,24 @@ int main() {
     const int32_t target = 50;
     std::atomic_int32_t current = 0;
     bot.on_message_create([&current](const dpp::message_create_t& event) {
+        auto content = std::string_view(event.msg.content);
+        if (content.empty()) return;
+        if (is_emote_string(content)) return;
+
         auto bot = event.from->creator;
-        auto content = event.msg.content;
-        if (content.empty()) {
-            return;
-        }
-        if (content == "mẹ thằng alc") {
+        if (content == "mẹ thằng alc")
+        {
             event.send("memaymem");
         }
 
-        if (content == "alc!uptime") {
+        if (content == "alc!uptime")
+        {
             auto uptime = bot->uptime();
             event.reply(uptime.to_string());
         }
         
-        if (event.msg.author.id == (std::uint64_t) 291569504104742922ULL && event.msg.guild_id == (std::uint64_t) 1027866055856619550) {
+        if (event.msg.author.id == (std::uint64_t) 291569504104742922ULL && event.msg.guild_id == (std::uint64_t) 1027866055856619550)
+        {
             current++;
             if (current >= target) {
                 event.reply("https://media.discordapp.net/attachments/1030315060737876020/1053190270809812992/alc.png");
